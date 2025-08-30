e6e8b074877204552b5f3fb689ceed1eab2727dc
Michael Gavalas <mgavalas23@gmail.com>
2025-08-30 18:13:14 +0300

    Added cuid support

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,12 @@
 import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { createId } from "@paralleldrive/cuid2";
 
+// Example
 export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
+  id: varchar("id", { length: 24 })
+    .primaryKey()
+    .$default(() => createId()),
+  username: varchar({ length: 255 }).notNull().unique(),
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
