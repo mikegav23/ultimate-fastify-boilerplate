42124eba8d903839650817f783f04b19942facc2
Michael Gavalas <mgavalas23@gmail.com>
2025-08-30 17:05:06 +0300

    DB client ssl to false in dev

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,8 +1,10 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
+import { config } from "../config.js";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: config.nodeEnv == "production" ? true : false,
 });
 
 const client = drizzle({ client: pool });
