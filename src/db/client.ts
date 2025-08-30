3c3a3c505b2dce0d4dc38286977e091575df2848
Michael Gavalas <mgavalas23@gmail.com>
2025-08-30 16:56:55 +0300

    Changed client config. Now using pooled connections with pg.Pool

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,10 @@
-import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
-const client = drizzle(process.env.DATABASE_URL!);
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const client = drizzle({ client: pool });
 
 export { client };
